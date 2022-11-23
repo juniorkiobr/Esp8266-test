83412cde5b520ad87bb830f09a5f1a2c7dbf6e4a
juniorkiobr <juniorkiolouco66@gmail.com>
2022-11-23 07:19:20 -0300

    adicionado readSensor.

--- npm/controllers/sensorsController.js
+++ npm/controllers/sensorsController.js
@@ -1,6 +1,26 @@
 const app = require('../controllers/mainController');
 // import { app } from '../controllers/mainController';
 const Sensor = require('../models/sensors');
+const WebSocketServer =  require("../controllers/webSocketController");
+
+var waitInterval = null;
+var resInterval = null;
+var bodyInterval = null;
+
+var waitArduinoResponse = function(){
+    if(WebSocketServer.getWaitArduinoResponse()){ return; }
+    bodyInterval.response_arduino = WebSocketServer.getLastArduinoMessage()
+    resInterval.send(JSON.stringify(bodyInterval));
+    clearInterval(waitInterval);
+    resInterval = null;
+    bodyInterval = null;
+}
+
+function delayArduinoResponse(res, body){
+    resInterval = res;
+    bodyInterval = body;
+    waitInterval = setInterval(waitArduinoResponse, 1000);
+}
 
 
 
@@ -8,6 +28,38 @@ const Sensor = require('../models/sensors');
 app.get("/sensors/getAll", async (req, res) =>{
     const sensors = await Sensor.findAll();
     res.send(sensors);
+});
+
+app.post("/interactions/writeInteraction", async (req, res) =>{
+    if(WebSocketServer.sendToArduino("cmd_ard_uno:>writeInteraction:" + req.body.id + ";" + req.body.value)    ){
+        console.log(req.body);
+        // await Interaction.update({valor: req.body.value }, {where: {id: req.body.id}});
+        let body = {
+            id: req.body.id,
+            value: req.body.value,
+            message: "Interacao atualizada com sucesso",            
+        }
+        delayArduinoResponse(res, body);
+    }else{
+        delayArduinoResponse(res, {message: "Erro: Arduino não conectado!"});
+    }
+
+});
+
+app.get("/sensors/readSensor", async (req, res) =>{
+
+    if(WebSocketServer.sendToArduino("cmd_ard_uno:>readSensor:" + req.body.id)    ){
+        console.log(req.body);
+        // await Interaction.update({valor: req.body.value }, {where: {id: req.body.id}});
+        let body = {
+            id: req.body.id,
+            value: req.body.value,
+            message: "Leitura realizada com sucesso",            
+        }
+        delayArduinoResponse(res, body);
+    }else{
+        delayArduinoResponse(res, {message: "Erro: Arduino não conectado!"});
+    }
 });
 
 app.put("/sensors/criarAmbiente", async (req, res) => {
